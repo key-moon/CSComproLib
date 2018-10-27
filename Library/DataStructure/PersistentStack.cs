4fdfe0da90d8f4b07a8f4c525e0d4278d6dfd195
key-moon <32513370+key-moon@users.noreply.github.com>
2018-10-27 11:02:52 +0900

    [add] 永続stackの追加

---
+++ Library/DataStructure/PersistentStack.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+class PersistentStack<T>
+{
+    PersistentStack<T> previousStack;
+    public T Top { get; private set; }
+    public int Count { get; private set; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PersistentStack() : this(null, default(T), 0) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private PersistentStack(PersistentStack<T> prev, T top, int count)
+    {
+        previousStack = prev;
+        Top = top;
+        Count = count;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PersistentStack<T> Push(T value)
+    {
+        var res = Copy();
+        res.previousStack = this;
+        res.Top = value;
+        res.Count++;
+        return res;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PersistentStack<T> Pop() => previousStack is null ? null : previousStack.Copy();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public PersistentStack<T> Copy() => new PersistentStack<T>(previousStack, Top, Count);
+}
