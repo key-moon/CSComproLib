13847aea9b66f09e039c4ee6ea19ef0fa9a391ac
key-moon <32513370+key-moon@users.noreply.github.com>
2018-10-29 22:05:05 +0900

    [change] ミリ秒表示に変更

--- Library/Verify/DataStructure/SuccientBitVector.cs
+++ Library/Verify/DataStructure/SuccientBitVector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ public static partial class Verify
             }
             var sucVB = new SuccinctBitVector(s);
 
-            Console.WriteLine($"start : {i}");
+            Debug.WriteLine($"start : {i}");
 
             start = DateTime.Now;
             for (int k = 0; k < vbIterate; k++) sucVB.Access(k);
@@ -42,9 +43,9 @@ public static partial class Verify
             spans[2] += end - start;
         }
         Console.WriteLine($"要素{vbSize}個\n{vbIterate}回合計({iterate}回平均)");
-        Console.WriteLine($"access : {(spans[0].Ticks / (iterate))}μs");
-        Console.WriteLine($"  rank : {(spans[1].Ticks / (iterate))}μs");
-        Console.WriteLine($"select : {(spans[2].Ticks / (iterate))}μs");
+        Console.WriteLine($"access : {(spans[0].TotalMilliseconds / (iterate)):0.000}ms");
+        Console.WriteLine($"  rank : {(spans[1].TotalMilliseconds / (iterate)):0.000}ms");
+        Console.WriteLine($"select : {(spans[2].TotalMilliseconds / (iterate)):0.000}ms");
     }
     public static void SucVB_Verify(int iterate, int vbMaxSize)
     {
@@ -52,7 +53,74 @@ public static partial class Verify
         {
             bool[] s = Enumerable.Repeat(0, vbMaxSize).Select(_ => RNG.NextBool()).ToArray();
             SuccinctBitVector sucVB = new SuccinctBitVector(s);
-            Console.WriteLine($"start : {i}");
+            Debug.WriteLine($"start : {i}");
+            int rank = 0;
+            int count0 = 0;
+            int count1 = 0;
+            for (int k = 0; k < vbMaxSize; k++)
+            {
+                var kind = s[k];
+                if (kind) rank++;
+
+                var accessres = sucVB.Access(k);
+                var rankres = sucVB.Rank(k);
+                var selectres = sucVB.Select(kind ? count1 : count0, kind);
+                if (kind != accessres) throw new Exception();
+                if (rank != rankres) throw new Exception();
+                if (k != selectres) throw new Exception();
+
+                if (kind) count1++;
+                else count0++;
+            }
+        }
+    }
+    public static void SucVB32_Measure(int iterate, int vbSize, int vbIterate)
+    {
+        DateTime start = new DateTime(), end = new DateTime();
+        TimeSpan[] spans = new TimeSpan[3];
+        for (int i = 0; i < iterate; i++)
+        {
+            //int n = 131070;
+            bool[] s = Enumerable.Repeat(0, vbSize).Select(_ => RNG.NextBool()).ToArray();
+            int count0 = 0;
+            int count1 = 0;
+            for (int k = 0; k < vbIterate; k++)
+            {
+                if (s[k]) count1++;
+                else count0++;
+            }
+            var sucVB = new SuccinctBitVector32(s);
+
+            Debug.WriteLine($"start : {i}");
+
+            start = DateTime.Now;
+            for (int k = 0; k < vbIterate; k++) sucVB.Access(k);
+            end = DateTime.Now;
+            spans[0] += end - start;
+
+            start = DateTime.Now;
+            for (int k = 0; k < vbIterate; k++) sucVB.Rank(k);
+            end = DateTime.Now;
+            spans[1] += end - start;
+
+            start = DateTime.Now;
+            for (int k = 0; k < count0; k++) sucVB.Select0(k);
+            for (int k = 0; k < count1; k++) sucVB.Select1(k);
+            end = DateTime.Now;
+            spans[2] += end - start;
+        }
+        Console.WriteLine($"要素{vbSize}個\n{vbIterate}回合計({iterate}回平均)");
+        Console.WriteLine($"access : {(spans[0].TotalMilliseconds / (iterate)):0.000}ms");
+        Console.WriteLine($"  rank : {(spans[1].TotalMilliseconds / (iterate)):0.000}ms");
+        Console.WriteLine($"select : {(spans[2].TotalMilliseconds / (iterate)):0.000}ms");
+    }
+    public static void SucVB32_Verify(int iterate, int vbMaxSize)
+    {
+        for (int i = 0; i < iterate; i++)
+        {
+            bool[] s = Enumerable.Repeat(0, vbMaxSize).Select(_ => RNG.NextBool()).ToArray();
+            SuccinctBitVector32 sucVB = new SuccinctBitVector32(s);
+            Debug.WriteLine($"start : {i}");
             int rank = 0;
             int count0 = 0;
             int count1 = 0;
